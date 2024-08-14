a21f90bab948dadc1dc33915b12adc2d3b14334f
Jan Baudisch <jan.baudisch@uni-ulm.de>
2024-08-14 22:46:26 +0200

    feat(nix): enable nix build

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "#SAT solver based on modern DPLL based SAT solver technology";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      sharpSAT = pkgs: pkgs.callPackage ./sharpSAT.nix { };
+    in
+    {
+      formatter = lib.genAttrs systems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      packages = lib.genAttrs systems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = self.packages.${system}.sharpSAT;
+
+          sharpSAT = sharpSAT pkgs;
+          sharpSAT-static = sharpSAT pkgs.pkgsStatic;
+          sharpSAT-windows = sharpSAT pkgs.pkgsCross.mingwW64;
+
+          container = pkgs.dockerTools.buildLayeredImage {
+            name = "sharpSAT";
+            contents = [ self.packages.${system}.sharpSAT ];
+            config = {
+              Entrypoint = [ "/bin/sharpSAT" ];
+              Labels = {
+                "org.opencontainers.image.source" = "https://github.com/SoftVarE-Group/sharpSAT";
+                "org.opencontainers.image.description" = "#SAT solver based on modern DPLL based SAT solver technology";
+                "org.opencontainers.image.licenses" = "MIT";
+              };
+            };
+          };
+        }
+      );
+    };
+}
---
+++ sharpSAT.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  stdenv,
+  fetchgit,
+  cmake,
+  gmp,
+}:
+stdenv.mkDerivation {
+  pname = "sharpSAT";
+  version = "13.02";
+
+  src = ./.;
+
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = [ gmp.dev ];
+
+  meta = with lib; {
+    mainProgram = "sharpSAT";
+    description = "#SAT solver based on modern DPLL based SAT solver technology";
+    homepage = "https://github.com/SoftVarE-Group/sharpSAT";
+    license = licenses.mit;
+    platforms = platforms.unix ++ platforms.windows;
+  };
+}
